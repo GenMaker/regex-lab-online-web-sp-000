c067ba92f2ac62dcb2f1c46970196243704fe35b
Panela Leung <leungpanela@gmail.com>
2019-08-17 20:10:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,15 +4,15 @@ def starts_with_a_vowel?(text)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.match(/^/)
+text.match(/^[un][ing$]/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\w+{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.scan(/^[A-Z]+/)
 end
 
 def valid_phone_number?(phone)
